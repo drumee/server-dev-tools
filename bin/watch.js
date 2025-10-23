c220d4315c3601e6cd560e8a159a945b699add03
Somanos Sar <somanos@drumee.com>
2025-10-23 12:41:27 +0700

    Changed watcher.js support ignored.txt

--- bin/watch.js
+++ bin/watch.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { resolve, join } = require("path");
-const { existsSync, watch } = require("fs");
+const { existsSync, readFileSync, watch } = require("fs");
 const { SRC_PATH } = process.env;
 const { exit } = process;
 const { exec } = require('shelljs');
@@ -19,12 +19,26 @@ if (!existsSync(argv.dir)) {
   console.log(`Could not watch ${argv.dir}, directory not found`);
   exit(1);
 }
-console.log(`Watching changes on ${argv.dir}`);
+const ignoreFile = join(argv.dir, '.dev-tools.rc/ignored.txt')
+let content = readFileSync(ignoreFile, 'utf8');
+content = content.replace(/[ \t]+/, '')
+
+let ignored = content.split('\n').map((e) => {
+  if (!e.length) return null;
+  let r = e.replace('*', '.*', 'g')
+  return new RegExp(r)
+})
+console.log(`Watching changes on ${argv.dir}`, ignored);
 let timer = null;
 watch(argv.dir, {
   encoding: 'utf8', recursive: true
 }, (e, filename) => {
-  if(timer) return;
+  for (let re of ignored) {
+    if (re && re.test(filename)) {
+      return
+    }
+  }
+  if (timer) return;
   timer = setTimeout(() => {
     let cmd = join(__dirname, "handler")
     console.log(`${cmd}`);
