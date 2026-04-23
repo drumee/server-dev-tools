d43499a40104d54ed1fb05b1f283f200b06d0b7f
Somanos Sar <somanos@drumee.net>
2026-04-24 00:06:32 +0700

    WIP

--- .dev-tools.tpl/reload.sh
+++ .dev-tools.tpl/reload.sh
@@ -5,8 +5,17 @@ if [ "$ENDPOINT" = "" ]; then
   echo Nothing to run after deploy
 else
   if [ "$CONTAINER_NAME" != "" ]; then
-    sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT
-    sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT/service
+     if [ "$DEST_HOST" = "" ]; then
+      sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT
+      sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT/service
+    else
+      if [ "$DEST_USER" != "" ]; then
+        ssh $DEST_USER@$DEST_HOST sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT
+        ssh $DEST_USER@$DEST_HOST sudo docker exec $CONTAINER_NAME drumee start $ENDPOINT/service
+      else 
+        echo Require DEST_USER to update remote
+      fi
+    fi
   else
     if [ "$DEST_HOST" = "" ]; then
       sudo drumee start $ENDPOINT
